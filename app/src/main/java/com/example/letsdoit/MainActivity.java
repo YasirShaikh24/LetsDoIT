fe29ab27e590de39131315962e225abd02e63b91
YasirShaikh24 <yasirazimshaikh5440@gmail.com>
2025-11-01 16:04:33 +0530

    navi

--- app/src/main/java/com/example/letsdoit/MainActivity.java
+++ app/src/main/java/com/example/letsdoit/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.letsdoit;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.navigation_add_activity) {
+                selectedFragment = new AddActivityFragment();
+            } else if (itemId == R.id.navigation_view_activity) {
+                selectedFragment = new ViewActivityFragment();
+            } else if (itemId == R.id.navigation_profile_activity) {
+                selectedFragment = new ProfileActivityFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
         });
+
+        // Set the Dashboard/Home Fragment as the default selected screen
+        if (savedInstanceState == null) {
+            bottomNav.setSelectedItemId(R.id.navigation_home);
+        }
     }
 }
